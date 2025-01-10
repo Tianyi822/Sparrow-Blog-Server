dca8e92e1bb41949f8caf91409088ce2afa8462b
Tianyi <chentyit@163.com>
2025-01-10 18:05:09 +0800

    删除Oss保存路径

--- internal/model/entity/imgInfo.go
+++ internal/model/entity/imgInfo.go
@@ -3,7 +3,6 @@ package entity
 type ImgInfo struct {
 	ImgId      int    `gorm:"column:img_id;primaryKey;autoIncrement"`                      // 图片ID
 	ImgName    string `gorm:"column:img_name;unique"`                                      // 图片名称
-	ImgOssPath string `gorm:"column:img_oss_path"`                                         // 图片的 OSS URL
 	CreateTime string `gorm:"column:create_time;default:CURRENT_TIMESTAMP"`                // 创建时间
 	UpdateTime string `gorm:"column:update_time;default:CURRENT_TIMESTAMP;autoUpdateTime"` // 更新时间
 }
