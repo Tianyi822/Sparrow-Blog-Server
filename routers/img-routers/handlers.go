3bc2fb21f2cdc006a8595e70afb44876b90b7d17
Tianyi <chentyit@163.com>
2025-01-10 18:31:58 +0800

    添加处理逻辑

--- routers/img-routers/handlers.go
+++ routers/img-routers/handlers.go
@@ -1,7 +1,17 @@
 package img_routers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"h2blog/pkg/resp"
+	"h2blog/routers/tools"
+)
 
 func uploadImages(ctx *gin.Context) {
+	// Get imgs data from raw data
+	imgsDto, err := tools.GetImgsDto(ctx)
+	if err != nil {
+		return
+	}
 
+	resp.Ok(ctx, "上传成功", imgsDto)
 }
