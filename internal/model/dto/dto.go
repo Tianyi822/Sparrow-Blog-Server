fdfb2eeb68387f425369ff16d472fdb455feb88e
Tianyi <chentyit@163.com>
2025-01-10 18:31:39 +0800

    添加图片批次dto

--- internal/model/dto/dto.go
+++ internal/model/dto/dto.go
@@ -1,41 +1,32 @@
 package dto
 
-// Dto is an interface that defines a contract for data transfer objects (DTOs).
-// It includes two methods that any type implementing this interface must provide.
+// Dto 是一个接口，定义了所有数据传输对象（DTO）必须实现的方法
 type Dto interface {
-	// DtoFlag returns a string that represents a flag or identifier for the DTO.
-	// This method is used to distinguish different types of DTOs or to provide
-	// additional metadata about the DTO.
+	// DtoFlag 方法返回一个字符串，用于标识 Dto 的某种特性或状态
 	DtoFlag() string
 
-	// Name returns a string that represents the name of the DTO.
-	// This method is typically used to get a human-readable name for the DTO,
-	// which can be useful for logging, debugging, or display purposes.
+	// Name 方法返回一个字符串，用于获取 Dto 的名称
 	Name() string
 }
 
-// BlogInfoDto is a struct that holds information about a blog post.
-// It includes fields for the blog ID, title, and a brief summary.
+// BlogInfoDto 用于表示博客信息的传输对象
 type BlogInfoDto struct {
 	BlogId string `json:"blog_id,omitempty"`
 	Title  string `json:"title,omitempty"`
 	Brief  string `json:"brief,omitempty"`
 }
 
-// DtoFlag returns a string identifier for the type of DTO.
-// This method is typically used to identify the type of data transfer object.
+// DtoFlag 方法返回 BlogInfoDto 的标识字符串
 func (bid *BlogInfoDto) DtoFlag() string {
 	return "BlogInfoDto"
 }
 
-// Name returns the title of the blog post.
-// This method provides a way to access the title field of the BlogInfoDto struct.
+// Name 方法返回 BlogInfoDto 的名称
 func (bid *BlogInfoDto) Name() string {
 	return bid.Title
 }
 
-// ImgDto is a struct that represents an image data transfer object.
-// This struct is used to encapsulate image-related data for transfer between different layers
+// ImgDto 图片数据
 type ImgDto struct {
 	ImgId   string `json:"img_id,omitempty"`
 	ImgName string `json:"img_name,omitempty"`
@@ -47,4 +38,17 @@ func (i *ImgDto) DtoFlag() string {
 
 func (i *ImgDto) Name() string {
 	return i.ImgName
+}
+
+// ImgsDto 图片数据
+type ImgsDto struct {
+	Imgs []ImgDto `json:"imgs,omitempty"`
+}
+
+func (is *ImgsDto) DtoFlag() string {
+	return "ImgsDto"
+}
+
+func (is *ImgsDto) Name() string {
+	return "ImgsDto"
 }
