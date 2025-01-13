d48f17da1ae570df7c3881f6a429c471af6da222
Tianyi <chentyit@163.com>
2025-01-13 20:19:06 +0800

    将所有 po 对象放到一个文件下

--- internal/model/po/po.go
+++ internal/model/po/po.go
@@ -1,8 +1,6 @@
 package po
 
-import (
-	"time"
-)
+import "time"
 
 type BlogInfo struct {
 	BlogId     string    `gorm:"column:blog_id;primaryKey;"`                                  // 博客ID
@@ -14,4 +12,15 @@ type BlogInfo struct {
 
 func (hbi *BlogInfo) TableName() string {
 	return "H2_BLOG_INFO"
+}
+
+type ImgInfo struct {
+	ImgId      string    `gorm:"column:img_id;primaryKey"`                                    // 图片ID
+	ImgName    string    `gorm:"column:img_name;unique"`                                      // 图片名称
+	CreateTime time.Time `gorm:"column:create_time;default:CURRENT_TIMESTAMP"`                // 创建时间
+	UpdateTime time.Time `gorm:"column:update_time;default:CURRENT_TIMESTAMP;autoUpdateTime"` // 更新时间
+}
+
+func (ii *ImgInfo) TableName() string {
+	return "H2_IMG_INFO"
 }
