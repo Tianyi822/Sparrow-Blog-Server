19880f84cf261d1fd82e2998bd8265628d6d4f71
Tianyi <chentyit@163.com>
2025-01-18 20:17:16 +0800

    添加批量图片信息的视图对象

--- internal/model/vo/vo.go
+++ internal/model/vo/vo.go
@@ -18,9 +18,17 @@ func (bv *BlogInfoVo) VoFlag() string {
 type ImgInfoVo struct {
 	ImgId   string `json:"img_id,omitempty"`
 	ImgName string `json:"img_name,omitempty"`
-	ImgType string `json:"img_type,omitempty"`
 }
 
 func (iv *ImgInfoVo) VoFlag() string {
 	return "ImgInfoVo"
+}
+
+type ImgInfosVo struct {
+	Success []ImgInfoVo `json:"success,omitempty"`
+	Fail    []ImgInfoVo `json:"fail,omitempty"`
+}
+
+func (isv *ImgInfosVo) VoFlag() string {
+	return "ImgInfosVo"
 }
