7ad757c981a0eecbf02997b0dfbd71835b716f24
Tianyi <chentyit@163.com>
2025-01-24 20:46:25 +0800

    添加生产 Hash 值的方法

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,12 +1,29 @@
 package utils
 
 import (
+	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
 	"github.com/google/uuid"
 	"golang.org/x/crypto/blake2b"
 	"h2blog/pkg/logger"
 )
+
+func GenHash(str string) string {
+	// 创建SHA256哈希对象
+	hash := sha256.New()
+
+	// 将字符串转换为字节数组并写入哈希对象
+	hash.Write([]byte(str))
+
+	// 计算哈希值
+	hashBytes := hash.Sum(nil)
+
+	// 将哈希值转换为十六进制字符串
+	hashStr := hex.EncodeToString(hashBytes)
+
+	return hashStr
+}
 
 // GenUUID 生成一个随机的 UUID
 func GenUUID() string {
