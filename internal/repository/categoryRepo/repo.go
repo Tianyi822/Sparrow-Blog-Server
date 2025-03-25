6c1f0b1d86b0c7ae5809daeea423742cc8f7bd08
Tianyi <chentyit@163.com>
2025-03-25 16:40:50 +0800

    feat: 增加分类数据添加操作

--- internal/repository/categoryRepo/repo.go
+++ internal/repository/categoryRepo/repo.go
@@ -4,10 +4,54 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"h2blog_server/internal/model/dto"
 	"h2blog_server/internal/model/po"
 	"h2blog_server/pkg/logger"
+	"h2blog_server/pkg/utils"
 	"h2blog_server/storage"
 )
+
+// AddCategory 添加一个新的播客分类到数据库中。
+// 参数：
+// - ctx context.Context: 上下文对象，用于控制请求的生命周期和传递元数据。
+// - cateDto *dto.CategoryDto: 包含分类信息的数据传输对象，其中 CName 是分类名称。
+//
+// 返回值：
+// - error: 如果操作成功，返回 nil；如果发生错误，返回包含错误信息的 error 对象。
+func AddCategory(ctx context.Context, cateDto *dto.CategoryDto) error {
+	tx := storage.Storage.Db.WithContext(ctx).Begin()
+	defer func() {
+		// 捕获可能的 panic，记录错误日志并回滚事务，避免事务未正确关闭。
+		if r := recover(); r != nil {
+			logger.Error("添加分类失败: %v", r)
+			tx.Rollback()
+		}
+	}()
+
+	// 根据分类名称生成唯一的分类 ID。如果生成失败，记录警告日志并返回错误。
+	cId, err := utils.GenId(cateDto.CName)
+	if err != nil {
+		msg := fmt.Sprintf("根据分类名称生成分类 ID 失败: %v", err)
+		logger.Warn(msg)
+		return errors.New(msg)
+	}
+
+	logger.Info("创建播客分类")
+	// 将生成的分类信息保存到数据库中。如果保存失败，记录警告日志并返回错误。
+	if err = tx.WithContext(ctx).Create(&po.Category{
+		CId:   cId,
+		CName: cateDto.CName,
+	}).Error; err != nil {
+		msg := fmt.Sprintf("创建分类失败: %v", err)
+		logger.Warn(msg)
+		return errors.New(msg)
+	}
+	logger.Info("创建分类成功")
+	tx.Commit()
+
+	// 如果所有操作成功，返回 nil 表示没有错误。
+	return nil
+}
 
 // FindCategoryById 根据分类ID查找分类信息。
 // 参数:
--- internal/repository/categoryRepo/repo_test.go
+++ internal/repository/categoryRepo/repo_test.go
@@ -2,6 +2,7 @@ package categoryRepo
 
 import (
 	"context"
+	"h2blog_server/internal/model/dto"
 	"h2blog_server/pkg/config"
 	"h2blog_server/pkg/logger"
 	"h2blog_server/storage"
@@ -26,5 +27,18 @@ func TestFindCategoryById(t *testing.T) {
 		t.Error(err)
 	} else {
 		t.Log(cate)
+	}
+}
+
+func TestAddCategory(t *testing.T) {
+	err := AddCategory(context.Background(), &dto.CategoryDto{
+		CId:   "cat006",
+		CName: "测试分类",
+	})
+
+	if err != nil {
+		t.Error(err)
+	} else {
+		t.Log("添加分类成功")
 	}
 }
