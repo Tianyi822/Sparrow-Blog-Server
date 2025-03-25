4cfe95e1de9e18988acc4f70f5992e8e52b05442
Tianyi <chentyit@163.com>
2025-03-25 17:29:22 +0800

    feat: 新增 tag 操作 - AddTags 批量添加标签到数据库 - AddBlogTagAssociation 用于将博客与标签的关联数据批量保存到数据库中

--- internal/repository/tagRepo/repo.go
+++ internal/repository/tagRepo/repo.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"h2blog_server/internal/model/dto"
 	"h2blog_server/internal/model/po"
 	"h2blog_server/pkg/logger"
+	"h2blog_server/pkg/utils"
 	"h2blog_server/storage"
 )
 
@@ -52,4 +54,97 @@ func FindTagsByBlogId(ctx context.Context, blogId string) ([]*po.Tag, error) {
 
 	// 返回查询到的标签列表。
 	return tags, nil
+}
+
+// AddTags 批量添加标签到数据库。
+// 参数:
+// - ctx: 上下文对象，用于控制请求的生命周期和传递上下文信息。
+// - tags: 包含标签信息的 DTO 列表，每个 DTO 包含标签名称 (TName)。
+//
+// 返回值:
+// - error: 如果操作成功则返回 nil，否则返回包含错误信息的 error 对象。
+func AddTags(ctx context.Context, tags []dto.TagDto) ([]dto.TagDto, error) {
+	tx := storage.Storage.Db.WithContext(ctx).Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	var tagPos []po.Tag
+	var newTags []dto.TagDto
+	for _, tag := range tags {
+		// 为每个标签生成唯一 ID
+		tId, err := utils.GenId(tag.TName)
+		if err != nil {
+			msg := fmt.Sprintf("生成标签 ID 失败: %v", err)
+			logger.Warn(msg)
+			return nil, errors.New(msg)
+		}
+
+		// 将生成的标签信息转换为持久化对象并存储到切片中
+		tagPos = append(tagPos, po.Tag{
+			TId:   tId,
+			TName: tag.TName,
+		})
+
+		// 将生成的标签信息转换为 DTO 并添加到新标签列表中
+		newTags = append(newTags, dto.TagDto{
+			TId:   tId,
+			TName: tag.TName,
+		})
+	}
+
+	// 记录日志并尝试批量保存标签数据到数据库
+	logger.Info("批量保存标签数据")
+	if err := tx.Create(&tagPos).Error; err != nil {
+		msg := fmt.Sprintf("创建标签数据失败: %v", err)
+		logger.Warn(msg)
+		return nil, errors.New(msg)
+	}
+	// 提交事务以完成数据库写入
+	tx.Commit()
+
+	logger.Info("批量保存标签数据成功")
+
+	return newTags, nil
+}
+
+// AddBlogTagAssociation 用于将博客与标签的关联数据批量保存到数据库中。
+// 参数说明：
+// - ctx: 上下文对象，用于控制请求的生命周期和传递上下文信息。
+// - blogId: 博客的唯一标识符，用于关联博客和标签。
+// - tags: 标签的 DTO（数据传输对象）切片，包含需要关联的标签信息。
+//
+// 返回值：
+// - error: 如果操作成功，则返回 nil；如果发生错误，则返回具体的错误信息。
+func AddBlogTagAssociation(ctx context.Context, blogId string, tags []dto.TagDto) error {
+	tx := storage.Storage.Db.WithContext(ctx).Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	var blogTags []po.BlogTag
+	for _, tag := range tags {
+		// 将标签 DTO 转换为 BlogTag 模型对象，并构建批量插入的数据。
+		blogTags = append(blogTags, po.BlogTag{
+			BId: blogId,
+			TId: tag.TId,
+		})
+	}
+
+	logger.Info("批量保存博客标签关联数据")
+	// 批量插入博客标签关联数据到数据库。
+	if err := tx.Create(&blogTags).Error; err != nil {
+		msg := fmt.Sprintf("创建博客标签关联数据失败: %v", err)
+		logger.Warn(msg)
+		return errors.New(msg)
+	}
+	// 提交事物
+	tx.Commit()
+	logger.Info("批量保存博客标签关联数据成功")
+
+	return nil
 }
--- internal/repository/tagRepo/repo_test.go
+++ internal/repository/tagRepo/repo_test.go
@@ -2,6 +2,7 @@ package tagRepo
 
 import (
 	"context"
+	"h2blog_server/internal/model/dto"
 	"h2blog_server/pkg/config"
 	"h2blog_server/pkg/logger"
 	"h2blog_server/storage"
@@ -32,5 +33,41 @@ func TestFindTagsByBlogId(t *testing.T) {
 
 	for _, tag := range tags {
 		t.Logf("tag: %v", tag)
+	}
+}
+
+func TestAddTags(t *testing.T) {
+	ctx := context.Background()
+
+	tags := []dto.TagDto{
+		{
+			TName: "tag00001",
+		},
+		{
+			TName: "tag00002",
+		},
+	}
+
+	_, err := AddTags(ctx, tags)
+
+	if err != nil {
+		t.Errorf("AddTags() error = %v", err)
+	}
+}
+
+func TestAddBlogTagAssociation(t *testing.T) {
+	ctx := context.Background()
+
+	err := AddBlogTagAssociation(ctx, "blog00003", []dto.TagDto{
+		{
+			TId: "e7c039f52925c96f",
+		},
+		{
+			TId: "b30fd680a54768d7",
+		},
+	})
+
+	if err != nil {
+		t.Errorf("AddBlogTagAssociation() error = %v", err)
 	}
 }
