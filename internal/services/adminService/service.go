ada0d856ac7e960184f1eba931ec18c8c57d2c72
Tianyi <chentyit@163.com>
2025-03-30 20:42:58 +0800

    在服务层创建事务对象，并将事务传递到数据层，每个业务单独对事务进行控制，而不是交由数据层单独控制

--- internal/services/adminService/service.go
+++ internal/services/adminService/service.go
@@ -6,6 +6,8 @@ import (
 	"h2blog_server/internal/repository/blogRepo"
 	"h2blog_server/internal/repository/categoryRepo"
 	"h2blog_server/internal/repository/tagRepo"
+	"h2blog_server/pkg/logger"
+	"h2blog_server/storage"
 )
 
 // UpdateOrAddBlog 更新或添加博客信息，并处理相关的分类和标签逻辑。
@@ -16,24 +18,36 @@ import (
 // 返回值:
 //   - error: 如果操作过程中发生错误，则返回具体的错误信息；否则返回 nil。
 func UpdateOrAddBlog(ctx context.Context, blogDto *dto.BlogDto) error {
+	// 开启事务
+	tx := storage.Storage.Db.WithContext(ctx).Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			logger.Error("更新或创建博客数据失败: %v", r)
+			tx.Rollback()
+		}
+	}()
+
 	// 如果 blogDto 中没有 CategoryId，则表示该分类是新的，需要新建分类。
 	if len(blogDto.CategoryId) == 0 {
 		categoryDto := dto.CategoryDto{
 			CategoryName: blogDto.Category.CategoryName,
 		}
-		err := categoryRepo.AddCategory(ctx, &categoryDto)
+		err := categoryRepo.AddCategory(tx, &categoryDto)
 		if err != nil {
+			tx.Rollback()
 			return err
 		}
 		blogDto.CategoryId = categoryDto.CategoryId
 	}
 
+	// 保存新建的标签
+	var newTags []dto.TagDto
+
 	// 检查 blogDto 中的标签 ID，如果标签没有 ID，则需要创建新的标签。
 	if len(blogDto.Tags) != 0 {
-		// 分离出有 ID 的标签和没有 ID 的标签。
-		var tagsWithId []dto.TagDto
+		// 分离不携带 ID 的标签和携带 ID 的标签
 		var tagsWithoutId []dto.TagDto
-
+		var tagsWithId []dto.TagDto
 		for _, tag := range blogDto.Tags {
 			if tag.TagId != "" {
 				tagsWithId = append(tagsWithId, tag)
@@ -44,8 +58,10 @@ func UpdateOrAddBlog(ctx context.Context, blogDto *dto.BlogDto) error {
 
 		// 如果存在没有 ID 的标签，则调用仓库方法批量创建新标签。
 		if len(tagsWithoutId) != 0 {
-			newTags, err := tagRepo.AddTags(ctx, tagsWithoutId)
+			var err error
+			newTags, err = tagRepo.AddTags(tx, tagsWithoutId)
 			if err != nil {
+				tx.Rollback()
 				return err
 			}
 			// 将有 ID 的标签和新创建的标签合并回 blogDto。
@@ -55,24 +71,32 @@ func UpdateOrAddBlog(ctx context.Context, blogDto *dto.BlogDto) error {
 
 	// 根据 blogDto 是否包含 BlogId 判断是新增博客还是更新博客。
 	if len(blogDto.BlogId) == 0 {
-		if err := blogRepo.AddBlog(ctx, blogDto); err != nil {
+		if err := blogRepo.AddBlog(tx, blogDto); err != nil {
+			tx.Rollback()
 			return err
 		}
 
 		// 建立标签与博客的关联关系
-		if err := tagRepo.AddBlogTagAssociation(ctx, blogDto.BlogId, blogDto.Tags); err != nil {
+		if err := tagRepo.AddBlogTagAssociation(tx, blogDto.BlogId, blogDto.Tags); err != nil {
+			tx.Rollback()
 			return err
 		}
 	} else {
-		if err := blogRepo.UpdateBlog(ctx, blogDto); err != nil {
+		if err := blogRepo.UpdateBlog(tx, blogDto); err != nil {
+			tx.Rollback()
 			return err
 		}
 
 		// 更新标签与博客的关联关系
-		if err := tagRepo.UpdateBlogTagAssociation(ctx, blogDto.BlogId, blogDto.Tags); err != nil {
+		if err := tagRepo.UpdateBlogTagAssociation(tx, blogDto.BlogId, blogDto.Tags); err != nil {
+			tx.Rollback()
 			return err
 		}
 	}
+
+	// 提交事务
+	tx.Commit()
+	logger.Info("完成博客的更新或创建操作")
 
 	return nil
 }
@@ -135,11 +159,17 @@ func DeleteBlog(ctx context.Context, id string) error {
 		return err
 	}
 
+	// 开启事务
+	tx := storage.Storage.Db.WithContext(ctx).Begin()
+
 	// 调用仓库方法根据ID删除博客。
-	err = blogRepo.DeleteBlogById(ctx, id)
+	err = blogRepo.DeleteBlogById(tx, id)
 	if err != nil {
+		tx.Rollback()
 		return err
 	}
+
+	// TODO: 删除博客标签关联数据
 
 	// 统计该分类下剩余的博客数量。
 	num, err := blogRepo.CalBlogsCountByCategoryId(ctx, categoryId)
@@ -149,13 +179,15 @@ func DeleteBlog(ctx context.Context, id string) error {
 
 	// 如果该分类下没有博客，则删除该分类。
 	if num == 0 {
-		err = categoryRepo.DeleteCategoryById(ctx, categoryId)
+		err = categoryRepo.DeleteCategoryById(tx, categoryId)
 		if err != nil {
+			tx.Rollback()
 			return err
 		}
 	}
 
-	// 遍历所有与博客关联的标签，检查每个标签是否还有其他博客关联。
+	// 遍历所有与博客关联的标签，检查每个标签是否还有其他博客关联
+	tagsWithoutBlog := make([]dto.TagDto, 0)
 	for _, tag := range tags {
 		num, err = tagRepo.CalBlogsCountByTagId(ctx, tag.TagId)
 		if err != nil {
@@ -164,12 +196,20 @@ func DeleteBlog(ctx context.Context, id string) error {
 
 		// 如果某个标签没有其他博客关联，则删除该标签。
 		if num == 0 {
-			err = tagRepo.DeleteTagById(ctx, tag.TagId)
-			if err != nil {
-				return err
-			}
+			tagsWithoutBlog = append(tagsWithoutBlog, tag)
 		}
 	}
+
+	if len(tagsWithoutBlog) != 0 {
+		err = tagRepo.DeleteTags(tx, tagsWithoutBlog)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	tx.Commit()
+	logger.Info("删除博客数据成功")
 
 	return nil
 }
@@ -177,12 +217,12 @@ func DeleteBlog(ctx context.Context, id string) error {
 // GetAllCategoriesAndTags 获取所有的分类和标签信息。
 //
 // 参数:
-// - ctx: 上下文对象，用于控制请求生命周期和传递上下文信息。
+//   - ctx: 上下文对象，用于控制请求生命周期和传递上下文信息。
 //
 // 返回值:
-// - []*dto.CategoryDto: 包含所有分类信息的 DTO 列表。
-// - []*dto.TagDto: 包含所有标签信息的 DTO 列表。
-// - error: 如果在获取分类或标签时发生错误，则返回具体的错误信息；否则返回 nil。
+//   - []*dto.CategoryDto: 包含所有分类信息的 DTO 列表。
+//   - []*dto.TagDto: 包含所有标签信息的 DTO 列表。
+//   - error: 如果在获取分类或标签时发生错误，则返回具体的错误信息；否则返回 nil。
 func GetAllCategoriesAndTags(ctx context.Context) ([]*dto.CategoryDto, []*dto.TagDto, error) {
 	categories, err := categoryRepo.GetAllCategories(ctx)
 	if err != nil {
@@ -198,19 +238,26 @@ func GetAllCategoriesAndTags(ctx context.Context) ([]*dto.CategoryDto, []*dto.Ta
 }
 
 func SetTop(ctx context.Context, id string) error {
-	err := blogRepo.SetTopById(ctx, id)
-	if err != nil {
+	tx := storage.Storage.Db.WithContext(ctx).Begin()
+
+	if err := blogRepo.SetTopById(tx, id); err != nil {
+		tx.Rollback()
 		return err
 	}
+
+	tx.Commit()
 
 	return nil
 }
 
 func ChangeBlogState(ctx context.Context, id string) error {
-	err := blogRepo.ChangeBlogStateById(ctx, id)
-	if err != nil {
+	tx := storage.Storage.Db.WithContext(ctx).Begin()
+
+	if err := blogRepo.ChangeBlogStateById(tx, id); err != nil {
 		return err
 	}
+
+	tx.Commit()
 
 	return nil
 }
