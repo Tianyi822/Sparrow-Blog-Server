eb5eda210b2570b21f5012ac05ab22eb7eb7d3ce
Tianyi <chentyit@163.com>
2025-04-01 19:34:01 +0800

    feat: 将缓存 key 值都放到 storage 包下

--- storage/common.go
+++ storage/common.go
@@ -1,5 +1,11 @@
 package storage
 
+// VerificationCodeKey 验证码缓存 key
+const VerificationCodeKey = "verification_code"
+
+// TokenCodeKey token 缓存 key
+const TokenCodeKey = "token_code_key"
+
 // ImgCacheKeyPrefix 图片缓存 key 前缀
 const ImgCacheKeyPrefix = "img_cache_"
 
