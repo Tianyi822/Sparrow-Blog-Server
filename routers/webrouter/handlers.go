d7623cd2d52c7a4b3717a5230738e883816e75fb
Tianyi <chentyit@163.com>
2025-04-21 11:34:13 +0800

    feat(routers): 更新用户基本信息接口并调整路由
    - 在 userBasicInfo 函数中增加了用户邮箱、背景图片、头像图片和网站 logo 等信息的返回
    - 将 /config 组路由重命名为 /web 组路由

--- routers/webrouter/handlers.go
+++ routers/webrouter/handlers.go
@@ -10,6 +10,10 @@ import (
 func userBasicInfo(ctx *gin.Context) {
 	// 构造并返回成功的 JSON 响应，包含用户名和用户邮箱信息。
 	resp.Ok(ctx, "获取成功", map[string]string{
-		"user_name": config.User.Username,
+		"user_name":        config.User.Username,
+		"user_email":       config.User.UserEmail,
+		"background_image": config.User.BackgroundImage,
+		"avatar_image":     config.User.AvatarImage,
+		"web_logo":         config.User.WebLogo,
 	})
 }
--- routers/webrouter/routers.go
+++ routers/webrouter/routers.go
@@ -3,7 +3,7 @@ package webrouter
 import "github.com/gin-gonic/gin"
 
 func Router(e *gin.Engine) {
-	configGroup := e.Group("/config")
+	configGroup := e.Group("/web")
 
 	// 获取用户基本信息
 	configGroup.GET("/user-basic-info", userBasicInfo)
