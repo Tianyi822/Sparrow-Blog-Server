e620bf5fe4a7763c3a0cac70a46d1b9bf438f1b4
Tianyi <chentyit@163.com>
2025-04-27 14:31:10 +0800

    feat(routers): 丰富用户基本信息接口返回内容
    - 在 userBasicInfo 函数中增加了用户 GitHub地址、用户爱好、打字机内容等字段
    - 修改返回值类型，以适应新增的字段

--- routers/webrouter/handlers.go
+++ routers/webrouter/handlers.go
@@ -9,11 +9,14 @@ import (
 // userBasicInfo 返回用户的基本信息。
 func userBasicInfo(ctx *gin.Context) {
 	// 构造并返回成功的 JSON 响应，包含用户名和用户邮箱信息。
-	resp.Ok(ctx, "获取成功", map[string]string{
-		"user_name":        config.User.Username,
-		"user_email":       config.User.UserEmail,
-		"background_image": config.User.BackgroundImage,
-		"avatar_image":     config.User.AvatarImage,
-		"web_logo":         config.User.WebLogo,
+	resp.Ok(ctx, "获取成功", map[string]any{
+		"user_name":           config.User.Username,
+		"user_email":          config.User.UserEmail,
+		"user_github_address": config.User.UserGithubAddress,
+		"user_hobbies":        config.User.UserHobbies,
+		"type_writer_content": config.User.TypeWriterContent,
+		"background_image":    config.User.BackgroundImage,
+		"avatar_image":        config.User.AvatarImage,
+		"web_logo":            config.User.WebLogo,
 	})
 }
