b1dc434cd6b84fe5cc88a8615b43678ccb748385
Tianyi <chentyit@163.com>
2025-05-02 16:51:33 +0800

    test(ossservice): 初始化测试环境并添加测试辅助函数
    - 添加测试环境初始化代码，包括加载配置文件、初始化 Logger 和数据库组件
    - 为 ossservice_test.go 文件添加测试辅助函数（暂无具体实现）

--- internal/services/sysservices/ossservice_test.go
+++ internal/services/sysservices/ossservice_test.go
@@ -2,9 +2,23 @@ package sysservices
 
 import (
 	"context"
+	"h2blog_server/pkg/config"
+	"h2blog_server/pkg/logger"
 	"h2blog_server/storage"
 	"testing"
 )
+
+func init() {
+	// 加载配置文件
+	_ = config.LoadConfig()
+	// 初始化 Logger 组件
+	err := logger.InitLogger(context.Background())
+	if err != nil {
+		return
+	}
+	// 初始化数据库组件
+	_ = storage.InitStorage(context.Background())
+}
 
 func TestGetPresignUrlById(t *testing.T) {
 	ctx := context.Background()
