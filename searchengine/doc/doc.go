638d7c406862ae129f6ffdd6bdda32b3f14d5c06
Tianyi <chentyit@163.com>
2025-06-10 22:55:43 +0800

    feat(doc): 在 Doc 结构中添加 Content 字段并更新 GetContent 方法
    - 在 Doc 结构中添加 Content 字段，用于存储文档内容
    - 更新 GetContent 方法，使其将获取的内容保存到 Doc 结构中
    - 修改相关测试以适应新的实现

--- searchengine/doc/doc.go
+++ searchengine/doc/doc.go
@@ -8,8 +8,9 @@ import (
 )
 
 type Doc struct {
-	ID    string // 文档 ID
-	Title string // 文档标题
+	ID      string // 文档 ID
+	Title   string // 文档标题
+	Content []byte // 文档内容
 }
 
 // GetContent 从OSS中获取文档的Markdown内容
@@ -17,14 +18,14 @@ type Doc struct {
 //   - ctx context.Context: 上下文对象，用于传递请求范围的 deadline、取消信号、认证信息等
 //
 // 返回值:
-//   - []byte: 文档的 Markdown 内容字节数组
 //   - error: 错误对象，如果获取内容时发生错误则返回
-func (d *Doc) GetContent(ctx context.Context) ([]byte, error) {
+func (d *Doc) GetContent(ctx context.Context) error {
 	logger.Info("从OSS中获取文档内容")
 	content, err := storage.Storage.GetContentFromOss(ctx, ossstore.GenOssSavePath(d.Title, ossstore.MarkDown))
 	if err != nil {
-		return nil, err
+		return err
 	}
+	d.Content = content
 
-	return content, nil
+	return nil
 }
