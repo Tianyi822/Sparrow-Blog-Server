490b3676825a92504b65086a66be4c3ed384623d
Tianyi <chentyit@163.com>
2025-10-16 16:21:15 +0800

    feat(blog): 新增最近七天博客阅读数查询功能

---
+++ internal/repositories/readcountrepo/readcountrepo.go
@@ -0,0 +1,42 @@
+package readcountrepo
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sparrow_blog_server/internal/model/dto"
+	"sparrow_blog_server/internal/model/po"
+	"sparrow_blog_server/pkg/logger"
+	"sparrow_blog_server/storage"
+	"time"
+)
+
+// GetRecentSevenDaysReadCount 查询最近七天博客阅读数
+func GetRecentSevenDaysReadCount(ctx context.Context) ([]*dto.BlogReadCountDto, error) {
+	logger.Info("查询最近七天博客阅读数")
+	var readCounts []*po.BlogReadCount
+	err := storage.Storage.Db.WithContext(ctx).Model(&po.BlogReadCount{}).
+		Select("sum(read_count) as read_count, read_date").
+		Where("read_date >= ?", time.Now().AddDate(0, 0, -7).Format("20060102")).
+		Group("read_date").
+		Order("read_date").
+		Scan(&readCounts).Error
+	if err != nil {
+		msg := fmt.Sprintf("数据库查询出错: %v", err.Error())
+		logger.Warn(msg)
+		return nil, errors.New(msg)
+	}
+	logger.Info("查询最近七天博客阅读数完成")
+
+	logger.Info("开始转换阅读数据为 DTO")
+	var readCountDtos []*dto.BlogReadCountDto
+	for _, readCount := range readCounts {
+		readCountDtos = append(readCountDtos, &dto.BlogReadCountDto{
+			ReadCount: readCount.ReadCount,
+			ReadDate:  readCount.ReadDate,
+		})
+	}
+	logger.Info("转换完成")
+
+	return readCountDtos, nil
+}
---
+++ internal/repositories/readcountrepo/readcountrepo_test.go
@@ -0,0 +1,31 @@
+package readcountrepo
+
+import (
+	"context"
+	"sparrow_blog_server/pkg/config"
+	"sparrow_blog_server/pkg/logger"
+	"sparrow_blog_server/storage"
+	"testing"
+)
+
+func init() {
+	// 加载配置文件
+	config.LoadConfig()
+	// 初始化 Logger 组件
+	err := logger.InitLogger(context.Background())
+	if err != nil {
+		return
+	}
+	// 初始化数据库组件
+	_ = storage.InitStorage(context.Background())
+}
+
+func TestGetRecentSevenDaysReadCount(t *testing.T) {
+	res, err := GetRecentSevenDaysReadCount(context.Background())
+	if err != nil {
+		t.Errorf("GetRecentSevenDaysReadCount error: %v", err)
+	}
+	for _, v := range res {
+		t.Logf("res: %v - %v", v.ReadCount, v.ReadDate)
+	}
+}
